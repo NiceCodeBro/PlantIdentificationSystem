5827a36fdd5c762f4d94abadd4c2e84e36973759
NiceCodeBro <m.seldur@gmail.com>
2017-12-22 19:00:27 +0300

    Arayüz düzenlendi

--- uploads/settings.py
+++ uploads/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+
+
     'uploads.core'
 ]
 
@@ -122,7 +124,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_DIRS = [
+#	os.path.join(BASE_DIR, 'static')
+#]
+#STATIC_ROOT = 	os.path.join(BASE_DIR, 'staticfiles/')
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
